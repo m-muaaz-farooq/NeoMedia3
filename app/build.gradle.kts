d66a7fe440063ee034527c3faa34f48ce2292e91
Muaaz Farooq <mmuaazfarooq786@gmail.com>
2025-03-14 22:48:09 +0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.muaaz.neomedia3"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.muaaz.neomedia3"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.google.errorprone.annotations)
+    implementation(libs.androidx.annotation)
+    compileOnly(libs.checker.qual)
+    compileOnly(libs.kotlin.annotations.jvm)
+}
